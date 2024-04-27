2136aa2fa5e89ab21a96206cd527950621d7b889
Roy Eilam <roye.busi@gmail.com>
2024-04-27 11:03:35 +0300

    Chancing neovim configurations

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -x
+set -e
 
 pushd ~
 USER_DIR=$(pwd)
 popd
 CONFIG_DIR=configs
-function prompt_question()
-{
+function prompt_question() {
     if [ $1 ]
     then
         read -p "$2 already installed, would you like to re-configure $2? (Y - yes, N - no)  " answer
@@ -15,51 +14,14 @@ function prompt_question()
     fi
 }
 
-function run_command()
-{
-	$@
-	if [ $? -ne 0 ]
-	then
-		echo "Cannot execute $@"
-		exit 1
-	fi
+function install_neovmim() {
+  local CONFIG_DIR=${USER_DIR}/.config/nvim
+
+  mkdir -p $CONFIG_DIR
+  git clone git@github.com:royeilam/neovimconfig.git $CONFIG_DIR
 }
 
-# Insalling necessary apps
-APP_LIST="neovim tmux curl python3 exuberant-ctags zsh git fonts-powerline nodejs npm fzf ripgrep"
-
-run_command sudo apt update
-run_command sudo apt upgrade
-run_command sudo apt -y install $APP_LIST
-
-# Configure neovim
-prompt_question "-d ${USER_DIR}/.config/nvim" "NEOVIM"
-
-if [ "$answer" == "Y" ]
-then
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-    run_command mkdir -p ${USER_DIR}/.config/nvim
-    run_command cp -f ${CONFIG_DIR}/nvim/init.vim ${USER_DIR}/.config/nvim/
-    run_command nvim +PlugInstall +qall
-    # Installing CoC
-    pushd ${USER_DIR}/.config/nvim/plugged/coc.nvim
-    # Installing YARN
-    run_command sudo npm install -g yarn
-    run_command yarn install
-    run_command yarn build
-    popd
-    run_command nvim +"CocInstall coc-pyright" +qa
-    run_command nvim +"CocInstall coc-clangd" +qa
-
-fi
-
-#Configure Zshell
-prompt_question "-d ${USER_DIR}/.oh-my-zsh" "OH-MY_ZSHELL"
-
-if [ $answer == "Y" ]
-then
+function install_oh_my_zsh() {
     if [ -d ${USER_DIR}/.oh-my-zsh ]
     then
         rm -rf ${USER_DIR}/.oh-my-zsh
@@ -67,11 +29,31 @@ then
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
     sed -i "s/^ZSH_THEME.*/ZSH_THEME=\"agnoster\"/" ${USER_DIR}/.zshrc
-    run_command git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    run_command git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     sed -i "s/plugins=(\(.*\))/plugins=(\1 zsh-autosuggestions zsh-syntax-highlighting)/g" ${USER_DIR}/.zshrc
     chsh -s $(which zsh)
+}
 
+# Insalling necessary apps
+APP_LIST="neovim tmux curl python3 exuberant-ctags zsh git fonts-powerline nodejs npm fzf ripgrep"
+
+sudo apt update
+sudo apt upgrade
+sudo apt -y install $APP_LIST
+
+# Configure neovim
+prompt_question "-d ${USER_DIR}/.config/nvim" "NEOVIM"
+
+if [ "$answer" == "Y" ]
+then
+    install_neovmim
 fi
 
-set +x
+#Configure Zshell
+prompt_question "-d ${USER_DIR}/.oh-my-zsh" "OH-MY_ZSHELL"
+
+if [ $answer == "Y" ]
+then
+  install_oh_my_zsh
+fi
